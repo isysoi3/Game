1fad0f3b682cd10009f430797e37ab523b6b3b0a
isysoi3 <14ilya12322@gmail.com>
2017-04-23 13:10:10 +0300

    Добавьте файлы проекта.

---
+++ Game/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    enum Gender{ male, female };
+
+    enum Race{ human, gnome };
+
+
+    class Hero
+    {
+        public static uint ID = 0;
+        private string name;
+        //состояние, возможность разговаривать, двигаться, раса,пол
+        private int age;
+        private int maxHealth;
+        private int currentHealth;
+        private int experience;
+    }
+}
