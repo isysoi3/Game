53e516d3ec627eb0127200157bfe6b8d36d3a365
isysoi3 <14ilya12322@gmail.com>
2017-04-23 13:53:08 +0300

    first

--- Game/Hero.cs
+++ Game/Hero.cs
@@ -1,22 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Game
 {
-    enum Gender{ male, female };
+    enum Gender { Male, Female };
+    enum Condition { Normal, Weakened, Sick, Poisoned, Paralyzed, Dead };
+    enum Race { Human, Gnome, Elf, Orc, Goblin };
 
-    enum Race{ human, gnome };
-
-
-    class Hero
+    class Hero : IComparable
     {
-        public static uint ID = 0;
-        private string name;
-        //состояние, возможность разговаривать, двигаться, раса,пол
-        private int age;
-        private int maxHealth;
-        private int currentHealth;
-        private int experience;
+        private static uint nextID = 1;//не изменяющ
+        public string name { get; private set; }//не изменяющ
+        public uint ID { get; private set; }
+        public Condition condition;
+        public bool isSpeaking;
+        public bool isMoving;
+        public Race race { get; private set; }//не изменяющ
+        public Gender gender { get; private set; }//не изменяющ
+        public int age;
+        public int maxHealth;
+        public int currentHealth;
+        public int experience { get; private set; }
+
+        public Hero(string aname, Race arace, Gender agender)
+        {
+            ID = nextID++;
+            name = aname;
+            race = arace;
+            gender = agender;
+        }
+
+        public Hero Compare(Hero a, Hero b)
+        {
+            return a.experience.CompareTo(b.experience);
+        }
+
+        public override string ToString()
+        {
+            return "Hero: " + name + " " + age + " " + race + " " + condition + " ";
+        }
+
     }
 }
